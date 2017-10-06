8b8b92d6e8ebfd8085ad2b1ba1958ccc278779c7
Nicholas Kern <nkern@gygax.aoc.nrao.edu>
2017-10-06 12:21:42 -0600

    modified:   run_notebook.sh

--- run_notebook.sh
+++ run_notebook.sh
@@ -4,10 +4,23 @@
 if [ $1 = '-h' ] || [ $1 = '--help' ]
 then
     echo 'Usage:'
-    echo 'run_notebook.sh <arg1: hera_data_dir>'
+    echo 'run_notebook.sh <arg1: hera_data_dir> <arg2: HERA_plots/ path>'
     echo ''
     echo 'argument1 : JD directory where visibility data *.uv files live'
+    echo 'argument2 : path to HERA_plots/ directory'
     exit 0
+fi
+
+if [ $1 = '']
+then
+    echo "arg1 is blank"
+    exit 1
+fi
+
+if [ $2 = '']
+then
+    echo "arg2 is blank"
+    exit 1
 fi
 
 # assign data directory
@@ -17,12 +30,12 @@ export DATA_PATH=$1
 jd=`python -c "print '${DATA_PATH}'.split('/')[-1]"`
 
 # get more env vars
-BASENBDIR=$HOME/HERA_plots
+BASENBDIR=$2
 OUTPUT=data_inspect_"$jd".ipynb
-OUTPUTDIR=$HOME/HERA_plots
+OUTPUTDIR=$2
 
 # copy and run notebook
-jupyter nbconvert --output=$OUTPUTDIR/$OUTPUT --to notebook --execute $BASENBDIR/Data_Inspect.ipynb
+jupyter nbconvert --output=$OUTPUTDIR/$OUTPUT --to notebook --ExecutePreprocessor.allow_errors=True --ExecutePreprocessor.timeout=-1 --execute $BASENBDIR/Data_Inspect.ipynb
 
 # cd to git repo
 cd $OUTPUTDIR
