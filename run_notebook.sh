204482e65c31994df1fd274c4c37a64d03a8261f
Peter Williams <peter@newton.cx>
2017-10-06 16:41:19 -0600

    run_notebook.sh: update to fetch data from Librarian

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,33 +1,60 @@
-#!/bin/bash 
+#!/bin/bash
 
 # print out help statement
 if [ $1 = '-h' ] || [ $1 = '--help' ]
 then
     echo 'Usage:'
-    echo 'run_notebook.sh <arg1: hera_data_dir> <arg2: HERA_plots/ path>'
+    echo 'run_notebook.sh <arg1: session ID> <arg2: HERA_plots/ path>'
     echo ''
-    echo 'argument1 : JD directory where visibility data *.uv files live'
+    echo 'argument1 : the ID of the session to process'
     echo 'argument2 : path to HERA_plots/ directory'
     exit 0
 fi
 
-if [ $1 = '']
-then
+if [ -z "$1" ] ; then
     echo "arg1 is blank"
     exit 1
 fi
 
-if [ $2 = '']
-then
+if [ -z "$2" ] ; then
     echo "arg2 is blank"
     exit 1
 fi
 
-# assign data directory
-export DATA_PATH=$1
+# Exit with an error if any sub-command fails.
 
-# get JD
-jd=`python -c "print '${DATA_PATH}'.split('/')[-1]"`
+set -e
+
+# Create a temporary Lustre directory for exporting the data and command the
+# Librarian to populate it.
+
+sessid="$1"
+staging_dir=$(mktemp -d --tmpdir=/lustre/aoc/projects/hera/nightlynb sessid$sessid.XXXXXX)
+search="{
+ \"session-id-is-exactly\": $sessid,
+ \"or\": {
+  \"name-matches\": \"%.HH.uv\",
+  \"name-matches\": \"%.HH.uvOR\",
+  \"name-matches\": \"%.json\",
+  \"name-matches\": \"%.calfits\"
+ }
+}
+"
+
+chmod ug+rwx "$staging_dir"
+librarian_stage_files.py --wait local "$staging_dir" "$search"
+
+DATA_PATH=
+
+for item in "$staging_dir"/2* ; do
+    if [ -n "$DATA_PATH" ] ; then
+        echo >&1 "WARNING: multiple subdirectories staged? $DATA_PATH, $item"
+        break
+    fi
+    export DATA_PATH="$item"
+done
+
+jd=$(basename $DATA_PATH)
 
 # get more env vars
 BASENBDIR=$2
@@ -35,7 +62,11 @@ OUTPUT=data_inspect_"$jd".ipynb
 OUTPUTDIR=$2
 
 # copy and run notebook
-jupyter nbconvert --output=$OUTPUTDIR/$OUTPUT --to notebook --ExecutePreprocessor.allow_errors=True --ExecutePreprocessor.timeout=-1 --execute $BASENBDIR/Data_Inspect.ipynb
+jupyter nbconvert --output=$OUTPUTDIR/$OUTPUT \
+  --to notebook \
+  --ExecutePreprocessor.allow_errors=True \
+  --ExecutePreprocessor.timeout=-1 \
+  --execute $BASENBDIR/Data_Inspect.ipynb
 
 # cd to git repo
 cd $OUTPUTDIR
@@ -44,3 +75,5 @@ cd $OUTPUTDIR
 git add $OUTPUT
 git commit -m "data inspect notebook for $jd"
 git push
+
+rm -rf "$staging_dir"
