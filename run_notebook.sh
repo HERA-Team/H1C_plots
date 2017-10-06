71f8d87ab69cb7c5f4ed55b11efc357ffb178315
Nicholas Kern <nkern@gygax.aoc.nrao.edu>
2017-10-05 18:08:41 -0600

    modified:   Data_Inspect.ipynb 	deleted:    hsa7458_v001.py 	modified:   run_notebook.sh

--- run_notebook.sh
+++ run_notebook.sh
@@ -3,10 +3,10 @@
 # print out help statement
 if [ $1 = '-h' ] || [ $1 = '--help' ]
 then
-    echo 'run_notebook.sh <arg1: hera_data_dir> <opt1: xx.uvc file> <opt2: yy.uvc file>'
-    echo 'argument1 : JD directory where visibility data *.uvc files live'
-    echo 'option1 : specific miriad uv file to use for xx pol'
-    echo 'option2 : specific miriad uv file to use for yy pol'
+    echo 'Usage:'
+    echo 'run_notebook.sh <arg1: hera_data_dir>'
+    echo ''
+    echo 'argument1 : JD directory where visibility data *.uv files live'
     exit 0
 fi
 
@@ -18,12 +18,8 @@ jd=`python -c "print '${DATA_PATH}'.split('/')[-1]"`
 
 # get more env vars
 BASENBDIR=$HOME/HERA_plots
-export CALFILE=hsa7458_v001
 OUTPUT=data_inspect_"$jd".ipynb
 OUTPUTDIR=$HOME/HERA_plots
-
-export UVFILE_XX=$2
-export UVFILE_YY=$3
 
 # copy and run notebook
 jupyter nbconvert --output=$OUTPUTDIR/$OUTPUT --to notebook --execute $BASENBDIR/Data_Inspect.ipynb
